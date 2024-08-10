0d439740329ca90a074914ceaf67e35b420f22de
Manoel Estevam Neto <104177851+estevam02009@users.noreply.github.com>
2024-08-10 12:27:20 -0300

    Criado componentes para produtos

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,16 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
         @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
-
     </head>
     <body>
-        <livewire:header />
         {{ $slot }}
-        <livewire:footer />
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Livewire\ProductDetails;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/details', ProductDetails::class);
